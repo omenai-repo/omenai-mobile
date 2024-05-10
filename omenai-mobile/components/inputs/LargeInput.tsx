4d3a1d8e4730d59eeb2f1e011024338af801a180
ifeanyicodes <ahumarezeifeanyi001@gmail.com>
2024-05-09 17:30:33 -0700

    Implemented UI for gallery signup

--- omenai-mobile/components/inputs/LargeInput.tsx
+++ omenai-mobile/components/inputs/LargeInput.tsx
@@ -1,4 +1,4 @@
-import { KeyboardTypeOptions, StyleSheet, Text, TextInput, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import { colors } from '@/config/colors.config'
 
@@ -7,12 +7,11 @@ type InputProps = {
     value: string,
     onInputChange: (e: string) => void,
     placeHolder: string,
-    keyboardType?: KeyboardTypeOptions,
     errorMessage?: string,
     handleBlur?: () => void
 }
 
-export default function Input({label, onInputChange, placeHolder, keyboardType, value, errorMessage, handleBlur}: InputProps) {
+export default function LargeInput({label, onInputChange, placeHolder, value, errorMessage, handleBlur}: InputProps) {
     return (
         <View>
             <Text style={styles.label}>{label}</Text>
@@ -20,11 +19,12 @@ export default function Input({label, onInputChange, placeHolder, keyboardType, 
                 onChangeText={onInputChange} 
                 placeholder={placeHolder} 
                 style={styles.inputContainer}
-                keyboardType={keyboardType}
+                keyboardType="default"
                 autoCapitalize="none"
                 value={value}
                 onBlur={handleBlur}
-                // autoComplete='email'
+                multiline
+                numberOfLines={4}
             />
             {errorMessage && errorMessage?.length > 0 && <Text style={styles.errorMessage}>{errorMessage}</Text>}
         </View>
@@ -37,12 +37,13 @@ const styles = StyleSheet.create({
         color: colors.inputLabel
     },
     inputContainer: {
-        height: 60,
+        height: 140,
         width: '100%',
         borderWidth: 1,
         borderColor: colors.inputBorder,
         backgroundColor: '#FAFAFA',
         paddingHorizontal: 20,
+        paddingTop: 15,
         borderRadius: 5,
         marginTop: 10
     },
