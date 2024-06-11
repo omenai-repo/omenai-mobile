f4ff4fa1b61c665a9fe62b76c37262c25eae67d6
Ahumareze <ahumarezeifeanyi001@gmail.com>
2024-06-11 16:29:00 +0100

    Integrated API for editorials screen and detailed screen

---
+++ omenai-mobile/secure/editorial/admin/controller/appwrite.ts
@@ -0,0 +1,12 @@
+import { Client, Account, ID, Storage, Databases } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.APPWRITE_EDITORIAL_PROJECT_ID!);
+
+export const storage = new Storage(client);
+export const database = new Databases(client);
+
+export { client, ID };
