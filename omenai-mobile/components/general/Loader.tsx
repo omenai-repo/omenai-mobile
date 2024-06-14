165a8d89d9259aec850f16fcaafeacff71a1e5bb
Ahumareze <ahumarezeifeanyi001@gmail.com>
2024-06-14 10:00:02 +0100

    Added custom loader animation

--- omenai-mobile/components/general/Loader.tsx
+++ omenai-mobile/components/general/Loader.tsx
@@ -1,10 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useRef } from 'react';
+import LottieView from 'lottie-react-native';
+import loaderAnimation from '../../assets/other/loader-animation.json';
 
 export default function Loader() {
+    const animation = useRef(null);
+
     return (
         <View style={styles.loadingContainer}>
-            <Text>Loading...</Text>
+            <LottieView
+                autoPlay
+                ref={animation}
+                style={{
+                    width: 200,
+                    height: 200
+                }}
+                source={loaderAnimation}
+            />
         </View>
     )
 }
