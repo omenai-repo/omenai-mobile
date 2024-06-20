50a0e2b4b4127332ec94af0aff2559f0db5803e5
ifeanyicodes <ifeanyi@omenai.net>
2024-06-20 09:19:11 +0100

    fixed modals update function

--- omenai-mobile/screens/register/Register.tsx
+++ omenai-mobile/screens/register/Register.tsx
@@ -8,26 +8,21 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { screenName } from '../../constants/screenNames.constants';
 import CustomModal from 'components/modal/CustomModal';
 import { useModalStore } from 'store/modal/modalStore';
+import WithModal from 'components/modal/WithModal';
 
 export default function Register() {
     const navigation = useNavigation<StackNavigationProp<any>>();
     const { modalMessage, setModalMessage } = useModalStore();
 
     return (
-        <View style={styles.container}>
+        <WithModal>
             <AuthHeader
                 title='Create an account'
                 subTitle='Fill in required details and create an account'
                 handleBackClick={() => navigation.navigate(screenName.welcome)}
             />
-            
             <FormController />
-            <CustomModal
-                value={modalMessage}
-                isVisible={modalMessage !== null}
-                handleDismiss={() => setModalMessage(null)}
-            />
-        </View>
+        </WithModal>
     )
 }
 
