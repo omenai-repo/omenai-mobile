1fd0a0fb3b9411913b74aee51a849d8580db9907
ifeanyicodes <ifeanyi@omenai.net>
2024-06-27 10:21:43 +0100

    Finished validator setup on screens

--- omenai-mobile/screens/uploadArtwork/components/Pricing.tsx
+++ omenai-mobile/screens/uploadArtwork/components/Pricing.tsx
@@ -1,23 +1,53 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { colors } from 'config/colors.config'
 import Input from 'components/inputs/Input'
 import CustomSelectPicker from 'components/inputs/CustomSelectPicker'
 import { displayPrice, preferredShippingCarrier } from 'data/uploadArtworkForm.data'
 import LongBlackButton from 'components/buttons/LongBlackButton'
-import { uploadArtworkStore } from 'store/gallery/uploadArtworkStore'
+import { uploadArtworkStore } from 'store/gallery/uploadArtworkStore';
+import { validate } from 'lib/validations/upload_artwork_input_validator/validator'
+
+type artworkPricingErrorsType = {
+    price: string
+}
 
 export default function Pricing() {
-    const {setActiveIndex, activeIndex} = uploadArtworkStore();
+    const {setActiveIndex, activeIndex, artworkUploadData, updateArtworkUploadData} = uploadArtworkStore();
+
+    const [formErrors, setFormErrors] = useState<artworkPricingErrorsType>({price: ''});
+
+    const checkIsDisabled = () => {
+        // Check if there are no error messages and all input fields are filled
+        const isFormValid = Object.values(formErrors).every((error) => error === "");
+        const areAllFieldsFilled = Object.values({
+            pricing: artworkUploadData.price,
+            showPrice: artworkUploadData.shouldShowPrice,
+            carrier: artworkUploadData.carrier
+        }).every((value) => value !== "");
+
+        return !(isFormValid && areAllFieldsFilled);
+    }
+
+    const handleValidationChecks = (label: string, value: string) => {        
+        const {success, errors} : {success: boolean, errors: string[] | []} = validate(label, value)
+        if(!success){
+            setFormErrors(prev => ({...prev, [label]: errors[0]}));
+        }else{
+            setFormErrors(prev => ({...prev, [label]: ''}));
+        }
+    };
 
     return (
         <View style={styles.container}>
             <View style={styles.inputsContainer}>
                 <Input
                     label='Price ($)'
-                    onInputChange={e => console.log(e)}
+                    onInputChange={value => updateArtworkUploadData('price', parseInt(value, 10))}
                     placeHolder='Enter your price'
-                    value=''
+                    value={artworkUploadData.price}
+                    handleBlur={() => handleValidationChecks('price', JSON.stringify(artworkUploadData.price))}
+                    errorMessage={formErrors.price}
                     keyboardType="decimal-pad"
                 />
                 <View style={{zIndex: 10}}>
@@ -25,17 +55,17 @@ export default function Pricing() {
                         label='Display price'
                         data={displayPrice}
                         placeholder='Select'
-                        value=''
-                        handleSetValue={e => console.log(e)}
+                        value={artworkUploadData.shouldShowPrice}
+                        handleSetValue={value => updateArtworkUploadData('shouldShowPrice', value)}
                     />
                 </View>
                 <View>
                     <CustomSelectPicker
                         label='Preferred shipping carrier'
                         data={preferredShippingCarrier}
-                        placeholder='Yes'
-                        value=''
-                        handleSetValue={e => console.log(e)}
+                        placeholder='Select carrier'
+                        value={artworkUploadData.carrier}
+                        handleSetValue={value => updateArtworkUploadData('carrier', value)}
                     />
                 </View>
             </View>
@@ -44,6 +74,7 @@ export default function Pricing() {
                     value='Proceed'
                     onClick={() => setActiveIndex(activeIndex + 1)}
                     isLoading={false}
+                    isDisabled={checkIsDisabled()}
                 />
             </View>
         </View>
