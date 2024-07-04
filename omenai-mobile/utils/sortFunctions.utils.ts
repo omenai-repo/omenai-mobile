71e8b0eb17dfa4215a3a3be23c77fef137a9c0b3
ifeanyicodes <ifeanyi@omenai.net>
2024-07-04 13:59:07 +0100

    updated order cards styling

--- omenai-mobile/utils/sortFunctions.utils.ts
+++ omenai-mobile/utils/sortFunctions.utils.ts
@@ -2,7 +2,7 @@ import { ordersColorsTypes } from "components/gallery/OrderCard";
 
 export const getColors = (selectedTab : string, order?: any): ordersColorsTypes => {
     if(selectedTab === 'processing' && order?.payment_information.status === "completed"){
-        return {bgColor: '#3E272320', textColor: '#000'}
+        return {bgColor: '#17963925', textColor: '#000'}
     }
     if(selectedTab === 'completed' &&  order?.order_accepted.status === "accepted"){
         return {bgColor: '#17963925', textColor: '#000'}
