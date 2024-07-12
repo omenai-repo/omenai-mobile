b6282dda0a1ec909e0f2a8165fa4fc1de299e75e
ifeanyicodes <ifeanyi@omenai.net>
2024-07-12 16:04:28 +0100

    Integrated API for stripe payout balance

--- omenai-mobile/screens/stripeScreens/payouts/StripePayouts.tsx
+++ omenai-mobile/screens/stripeScreens/payouts/StripePayouts.tsx
@@ -7,25 +7,28 @@ import { checkIsStripeOnboarded } from 'services/stripe/checkIsStripeOnboarded'
 import { useRoute } from '@react-navigation/native'
 import Loader from 'components/general/Loader'
 import CompleteOnBoarding from './components/CompleteOnBoarding'
+import { useModalStore } from 'store/modal/modalStore'
 
 export default function StripePayouts() {
     const route = useRoute();
     const [loading, setLoading] = useState(false);
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [accountID, setAccountID] = useState('');
+
+    const { updateModal } = useModalStore()
 
 
     useEffect(() => {
         async function handleOnBoardingCheck(){
             setLoading(true)
             const { account_id } = route.params as {account_id: string}
+            setAccountID(account_id)
             const res = await checkIsStripeOnboarded(account_id);
             if(res?.isOk){
                 setIsSubmitted(res.details_submitted)
             }else{
-                console.log('error')
+                updateModal({message: 'Something went wrong, please try again or contact support', modalType: 'error', showModal: true})
             }
-
-            console.log(res)
             setLoading(false)
         };
 
@@ -41,7 +44,8 @@ export default function StripePayouts() {
         <WithModal>
             <BackHeaderTitle title={isSubmitted ? 'Stripe Payout': 'Complete stripe onboarding'} />
             <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-                {isSubmitted ? <BalanceBox /> : <CompleteOnBoarding />}
+                {!isSubmitted && <CompleteOnBoarding />}
+                {(isSubmitted && accountID.length > 0) && <BalanceBox account_id={accountID} />}
             </ScrollView>
         </WithModal>
     )
