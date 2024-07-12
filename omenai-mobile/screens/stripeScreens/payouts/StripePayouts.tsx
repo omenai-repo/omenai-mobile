4091dd7a414c3fca619913d2fd9bc3a78cc79739
ifeanyicodes <ifeanyi@omenai.net>
2024-07-12 15:38:23 +0100

    feat: added connect account to stripe payout

--- omenai-mobile/screens/stripeScreens/payouts/StripePayouts.tsx
+++ omenai-mobile/screens/stripeScreens/payouts/StripePayouts.tsx
@@ -1,14 +1,57 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import WithModal from 'components/modal/WithModal'
+import BackHeaderTitle from 'components/header/BackHeaderTitle'
+import BalanceBox from './components/BalanceBox'
+import { checkIsStripeOnboarded } from 'services/stripe/checkIsStripeOnboarded'
+import { useRoute } from '@react-navigation/native'
+import Loader from 'components/general/Loader'
+import CompleteOnBoarding from './components/CompleteOnBoarding'
 
 export default function StripePayouts() {
+    const route = useRoute();
+    const [loading, setLoading] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+    useEffect(() => {
+        async function handleOnBoardingCheck(){
+            setLoading(true)
+            const { account_id } = route.params as {account_id: string}
+            const res = await checkIsStripeOnboarded(account_id);
+            if(res?.isOk){
+                setIsSubmitted(res.details_submitted)
+            }else{
+                console.log('error')
+            }
+
+            console.log(res)
+            setLoading(false)
+        };
+
+        handleOnBoardingCheck()
+    }, []);
+
+    if(loading)return(
+        <Loader />
+    )
+    
+    if(!loading)
     return (
-        <View>
-            <SafeAreaView>
-            <Text>Stripe Payouts</Text>
-            </SafeAreaView>
-        </View>
+        <WithModal>
+            <BackHeaderTitle title={isSubmitted ? 'Stripe Payout': 'Complete stripe onboarding'} />
+            <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+                {isSubmitted ? <BalanceBox /> : <CompleteOnBoarding />}
+            </ScrollView>
+        </WithModal>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 20,
+        flex: 1,
+        paddingTop: 15,
+        marginTop: 15
+    }
+})
