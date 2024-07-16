ebd775b6dd630916d353943d4e5baa1e0c7658a3
ifeanyicodes <ifeanyi@omenai.net>
2024-07-16 12:17:50 +0100

    integrated API for artwork lock

---
+++ omenai-mobile/services/orders/checkLockStatus.ts
@@ -0,0 +1,17 @@
+import { apiUrl } from "../../constants/apiUrl.constants";
+
+export const checkLockStatus = async (art_id: string, user_id: string) => {
+  try {
+    const res = await fetch(`${apiUrl}/api/locks/checkLock`, {
+      method: "POST",
+      body: JSON.stringify({
+        art_id,
+        user_id,
+      }),
+    });
+    const result = await res.json();
+    return { isOk: res.ok, message: result.message, data: result.data };
+  } catch (error: any) {
+    console.log(error);
+  }
+};
