beec7abbe95dfe293e357379e09c2a8c03058118
ifeanyicodes <ifeanyi@omenai.net>
2024-08-08 13:08:20 +0100

    integrate API to fetch single plan

---
+++ omenai-mobile/services/subscriptions/getSinglePlanData.ts
@@ -0,0 +1,19 @@
+import { apiUrl } from "../../constants/apiUrl.constants";
+
+export async function getSinglePlanData(plan_id: string) {
+  try {
+    const res = await fetch(`${apiUrl}/api/subscriptions/retrieveSinglePlan`, {
+      method: "POST",
+      body: JSON.stringify({ plan_id }),
+    });
+
+    const result = await res.json();
+
+    return {
+      isOk: res.ok,
+      data: result.data,
+    };
+  } catch (error: any) {
+    console.log(error);
+  }
+}
