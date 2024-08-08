cb1516970a2331ec5fbe4ecbe992fe50a953096d
ifeanyicodes <ifeanyi@omenai.net>
2024-08-08 16:43:42 +0100

    integrated direct charge flutterwave endpoint

--- omenai-mobile/screens/checkout/components/forms/CardInfo.tsx
+++ omenai-mobile/screens/checkout/components/forms/CardInfo.tsx
@@ -4,6 +4,14 @@ import Input from 'components/inputs/Input';
 import LongBlackButton from 'components/buttons/LongBlackButton';
 import { Entypo, Feather, Fontisto } from '@expo/vector-icons';
 import { colors } from 'config/colors.config';
+import { generateAlphaDigit } from 'utils/generateToken';
+import { hasEmptyString } from 'utils/hasEmptyString';
+import { useAppStore } from 'store/app/appStore';
+import { initiateDirectCharge } from 'services/subscriptions/subscribeUser/initiateDirectCharge';
+import { useModalStore } from 'store/modal/modalStore';
+import { apiUrl } from 'constants/apiUrl.constants';
+import { subscriptionStepperStore } from 'store/subscriptionStepper/subscriptionStepperStore';
+import { useRoute } from '@react-navigation/native';
 
 type cardInfoProps = {
     name: string,
@@ -20,7 +28,65 @@ type CardInfoProps = {
 }
 
 export default function CardInfo({handleNext, plan, updateAuthorization}:CardInfoProps) {
+    const routes = useRoute();
+    const { userSession } = useAppStore();
+    const { updateModal } = useModalStore();
+
+    const { tab }  = routes.params as {tab: string}
+
+    const { update_flw_charge_payload_data } = subscriptionStepperStore()
+
     const [cardInfo, setCardInfo] = useState<cardInfoProps>({name: '', cardNumber: '', expiryMonth: '', year: '', cvv: ''});
+    const [cardInputLoading, setCardInputLoading] = useState(false);
+
+    const handleCardSubmit = async () => {
+        setCardInputLoading(true)
+        const ref = generateAlphaDigit(7);
+        if (hasEmptyString(cardInfo)){
+            //theow error
+        }else{
+            const data: FLWDirectChargeDataTypes & { name: string } = {
+                name: cardInfo.name,
+                cvv: cardInfo.cvv,
+                card: cardInfo.cardNumber,
+                month: cardInfo.expiryMonth,
+                year: cardInfo.year.slice(2, 4),
+                tx_ref: ref,
+                amount: tab === "monthly" ? plan.pricing.monthly_price : plan.pricing.annual_price,
+                customer: {
+                  name: userSession.name,
+                  email: userSession.email,
+                },
+                redirect: `${apiUrl}/dashboard/gallery/billing`,
+            };
+
+            const response = await initiateDirectCharge(data);
+            if (response?.isOk) {
+                if (response.data.status === "error") {
+                    updateModal({message: response.data.message, showModal: true, modalType: 'error'})
+                } else {
+                console.log(response.data);
+                if (response.data.meta.authorization.mode === "redirect") {
+                    // console.log("User needs to be redirected");
+                    handleRedirect(response.data.meta.authorization.redirect);
+                    // redirect user
+                } else {
+                    updateAuthorization(response.data.meta.authorization.mode);
+                }
+                handleNext();
+                }
+                update_flw_charge_payload_data(data);
+            } else {
+                updateModal({message: 'Something went wrong', showModal: true, modalType: 'error'})
+            }
+        }
+
+        setCardInputLoading(false)
+    };
+
+    const handleRedirect = (path: string) => {
+        // redirect to 
+    }
 
     return (
         <View>
@@ -43,6 +109,7 @@ export default function CardInfo({handleNext, plan, updateAuthorization}:CardInf
                     onInputChange={e => setCardInfo(prev => ({...prev, cardNumber: e}))}
                     value={cardInfo.cardNumber}
                     placeHolder='Enter the card number'
+                    keyboardType="number-pad"
                 />
                 <View style={{flexDirection: 'row', gap: 20}}>
                     <View style={{flex: 1}}>
@@ -51,6 +118,7 @@ export default function CardInfo({handleNext, plan, updateAuthorization}:CardInf
                             onInputChange={e => setCardInfo(prev => ({...prev, expiryMonth: e}))}
                             value={cardInfo.expiryMonth}
                             placeHolder='MM'
+                            keyboardType="number-pad"
                         />
                     </View>
                     <View style={{flex: 1}}>
@@ -59,6 +127,7 @@ export default function CardInfo({handleNext, plan, updateAuthorization}:CardInf
                             onInputChange={e => setCardInfo(prev => ({...prev, year: e}))}
                             value={cardInfo.year}
                             placeHolder='YYYY'
+                            keyboardType="number-pad"
                         />
                     </View>
                 </View>
@@ -67,11 +136,13 @@ export default function CardInfo({handleNext, plan, updateAuthorization}:CardInf
                     onInputChange={e => setCardInfo(prev => ({...prev, cvv: e}))}
                     value={cardInfo.cvv}
                     placeHolder='Enter the CVV number'
+                    keyboardType="number-pad"
                 />
             </View>
             <LongBlackButton
-                onClick={handleNext}
+                onClick={handleCardSubmit}
                 value='Submit'
+                isLoading={cardInputLoading}
             />
         </View>
     )
---
+++ omenai-mobile/store/subscriptionStepper/subscriptionStepperStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+
+type subscriptionStepperStoreTypes = {
+  avs_input_fields: string[];
+  updateAvsFields: (fields: string[]) => void;
+  flw_charge_payload: FLWDirectChargeDataTypes & { name: string };
+  update_flw_charge_payload_data: (
+    data: FLWDirectChargeDataTypes & { name: string }
+  ) => void;
+  flw_ref: string;
+  set_flw_ref: (ref: string) => void;
+};
+
+export const subscriptionStepperStore = create<subscriptionStepperStoreTypes>((set, get) => ({
+  avs_input_fields: [],
+  updateAvsFields: (fields: string[]) => {
+    set({ avs_input_fields: fields });
+  },
+  // authorization: {} as AuthorizationData,
+  // updateAuthorizationData: (data: AuthorizationData) => {
+  //   set({ authorization: data });
+  // },
+  flw_charge_payload: {} as FLWDirectChargeDataTypes & { name: string },
+  update_flw_charge_payload_data: (
+    data: FLWDirectChargeDataTypes & { name: string }
+  ) => {
+    set({ flw_charge_payload: data });
+  },
+  flw_ref: "",
+  set_flw_ref: (ref: string) => {
+    set({ flw_ref: ref });
+  },
+}));
