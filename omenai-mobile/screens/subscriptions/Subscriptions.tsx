e0cadedac347fc2690654fbddb2e604e184b7bd8
ifeanyicodes <ifeanyi@omenai.net>
2024-08-22 12:57:22 +0100

    updated subscription status checker functionality

--- omenai-mobile/screens/subscriptions/Subscriptions.tsx
+++ omenai-mobile/screens/subscriptions/Subscriptions.tsx
@@ -1,13 +1,47 @@
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { colors } from 'config/colors.config'
 import InActiveSubscription from './features/InActiveSubscription';
 import { useAppStore } from 'store/app/appStore';
 import ActiveSubscriptions from './features/ActiveSubscriptions';
 import WithModal from 'components/modal/WithModal';
+import ActiveSubLoader from './components/ActiveSubLoader';
+import { useModalStore } from 'store/modal/modalStore';
+import { retrieveSubscriptionData } from 'services/subscriptions/retrieveSubscriptionData';
+import { useIsFocused } from '@react-navigation/native';
 
 export default function Subscriptions() {
+    const isFocused = useIsFocused();
+
     const { userSession } = useAppStore();
+    const { updateModal } = useModalStore();
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [isSubActive, setIsSubActive] = useState(false);
+    const [ subscriptionData, setSubscriptionData ] = useState<any>();
+    
+
+    useEffect(() => {
+        async function handleFetchSubData(){
+            setLoading(true);
+
+            const res = await retrieveSubscriptionData(userSession.id);
+            
+            if(res?.isOk && res.data !== (null || undefined)){
+                setIsSubActive(true)
+                setSubscriptionData(res.data);
+            }else{
+                //something went wrong
+                updateModal({message: 'something went wrong', modalType: 'error', showModal: true})
+            }
+
+            setLoading(false)
+        }
+
+        if(isFocused){
+            handleFetchSubData()
+        }
+    }, [isFocused]);
 
     return (
         <WithModal>
@@ -16,13 +50,21 @@ export default function Subscriptions() {
                     <Text style={{fontSize: 20, textAlign: 'center'}}>Subscription & Billing</Text>
                 </View>
             </SafeAreaView>
+            {loading ?
+                <View style={{padding: 20}}>
+                    <ActiveSubLoader />
+                </View>
+            :
             <ScrollView style={styles.mainContainer} showsVerticalScrollIndicator={false}>
-                {userSession.subscription_active ?
-                    <ActiveSubscriptions />
+                {isSubActive ?
+                    <ActiveSubscriptions
+                        subscriptionData={subscriptionData}
+                    />
                 :
                     <InActiveSubscription />
                 }
             </ScrollView>
+}
         </WithModal>
     )
 }
