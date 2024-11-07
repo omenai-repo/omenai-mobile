f1d180179d111e8c3c7fbd5d5630a0f20d801244
ahmadadejumo <adejumoahmad@gmail.com>
2024-11-07 13:24:32 +0100

    test blank issue fix

--- lib/storage/getPromotionalsFileView.ts
+++ lib/storage/getPromotionalsFileView.ts
@@ -12,6 +12,15 @@ export const getPromotionalFileView = (
 
     width, // width, will be resized using this value.
     height ? height : 0, // height, ignored when 0
+    "center", // crop center
+    90, // slight compression
+    0, // border width
+    "FFFFFF", // border color
+    0, // border radius
+    1, // full opacity
+    0, // no rotation
+    "FFFFFF", // background color
+    format ? format : "webp"
   );
 
   return fileData.href;
