de05ad71ae36f1ba3652c38fb737e0d284c0fbeb
ahmadadejumo <adejumoahmad@gmail.com>
2024-11-08 17:22:49 +0100

    test blank issue fix

--- lib/storage/getPromotionalsFileView.ts
+++ lib/storage/getPromotionalsFileView.ts
@@ -8,7 +8,7 @@ export const getPromotionalFileView = (
   format?: string
 ) => {
   const fileData = promotional_storage.getFilePreview(
-    "66a0e9b3001b6f875e63",
+    process.env.EXPO_PUBLIC_APPWRITE_PROMOTIONAL_BUCKET_ID!,
     fileId,
 
     width, // width, will be resized using this value.
