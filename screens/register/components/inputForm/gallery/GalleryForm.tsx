792eafa9144449d0307eb6abeea2a4869ffdcbff
ahmadadejumo <adejumoahmad@gmail.com>
2024-11-15 16:34:32 +0100

    add new signup UI

--- screens/register/components/inputForm/gallery/GalleryForm.tsx
+++ screens/register/components/inputForm/gallery/GalleryForm.tsx
@@ -1,36 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import GalleryWaitList from '../../galleryWaitlist/GalleryWaitList'
-import GalleryRegisterForm from '../../galleryRegisterForm/GalleryRegisterForm'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import LongBlackButton from '../../../../../components/buttons/LongBlackButton'
-import Input from '../../../../../components/inputs/Input'
-import { UseGalleryAuthStore } from '../../../../../store/auth/login/galleryAuthStore'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import GalleryWaitList from "../../galleryWaitlist/GalleryWaitList";
+import GalleryRegisterForm from "../../galleryRegisterForm/GalleryRegisterForm";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import LongBlackButton from "../../../../../components/buttons/LongBlackButton";
+import Input from "../../../../../components/inputs/Input";
+import { UseGalleryAuthStore } from "../../../../../store/auth/login/galleryAuthStore";
 
 export default function GalleryForm() {
-    const [showWaitlistForm, setShowWaitlistForm] = useState<boolean>(false);
+  const [showWaitlistForm, setShowWaitlistForm] = useState<boolean>(false);
 
-    return(
-        <View style={styles.container}>
-            {showWaitlistForm ? <GalleryWaitList /> : <GalleryRegisterForm />}
-            <TouchableOpacity style={styles.toggleButton} onPress={() => setShowWaitlistForm(prev => !prev)}>
-                <Text style={styles.toggleText}>{showWaitlistForm ? 'Register gallery account' : 'Join our waitlist'}</Text>
-            </TouchableOpacity>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      {showWaitlistForm ? <GalleryWaitList /> : <GalleryRegisterForm />}
+      <TouchableOpacity
+        style={styles.toggleButton}
+        onPress={() => setShowWaitlistForm((prev) => !prev)}
+      >
+        <Text style={styles.toggleText}>
+          {showWaitlistForm ? "Register gallery account" : "Join our waitlist"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 40,
-        paddingBottom: 100
-    },
-    toggleButton: {
-        marginTop: 40,
-        paddingVertical: 10
-    },
-    toggleText: {
-        textAlign: 'center',
-        opacity: 0.8
-    }
-})
+  container: {
+    marginTop: 40,
+    paddingBottom: 100,
+  },
+  toggleButton: {
+    marginTop: 40,
+    paddingVertical: 10,
+  },
+  toggleText: {
+    textAlign: "center",
+    opacity: 0.8,
+  },
+});
