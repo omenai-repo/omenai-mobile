76fd84b91bac5baa240a4641fcd65d87c2e0ab2b
ifeanyicodes <ifeanyi@omenai.net>
2024-11-18 15:08:41 +0100

    updated components styling

--- components/artwork/MiniArtworkCard.tsx
+++ components/artwork/MiniArtworkCard.tsx
@@ -45,7 +45,7 @@ export default function MiniArtworkCard({
   const [renderDynamicImage, setRenderDynamicImage] = useState(false);
 
   let imageWidth = 0;
-  imageWidth = Math.round((screenWidth - 60) / 2); //screen width minus paddings applied to grid view tnen divided by two, to get the width of a single card
+  imageWidth = Math.round((screenWidth - 50) / 2); //screen width minus paddings applied to grid view tnen divided by two, to get the width of a single card
   const image_href = getImageFileView(url, imageWidth);
 
   useEffect(() => {
@@ -130,7 +130,7 @@ export default function MiniArtworkCard({
       style={tw`w-full bg-[#E7D9D9] rounded-2xl px-[10px] py-[10px] pb-[20px]`}
       onPress={() => navigation.push(screenName.artwork, { title: title })}
     >
-      <View style={tw`rounded-2xl overflow-hidden relative`}>
+      <View style={tw`rounded-[10px] overflow-hidden relative`}>
         {renderDynamicImage ? (
           <Image
             source={{ uri: image_href }}
@@ -138,7 +138,7 @@ export default function MiniArtworkCard({
               width: imageDimensions.width - 20,
               height: imageDimensions.height,
               objectFit: "cover",
-              borderRadius: 20,
+              borderRadius: 10,
               overflow: 'hidden'
             }}
             resizeMode="contain"
@@ -161,22 +161,24 @@ export default function MiniArtworkCard({
         )}
         <View style={tw`absolute top-0 left-0 h-full w-full flex items-end justify-start p-2`}>
         {galleryView && (
-          <View style={tw`bg-white h-[30px] w-[30px] rounded-full flex items-center justify-center`}>
+          <View style={tw`bg-white/20 h-[30px] w-[30px] rounded-full flex items-center justify-center`}>
             <LikeComponent
               art_id={art_id}
               impressions={impressions || 0}
               likeIds={like_IDs || []}
+              lightText
             />
           </View>
         )}
         </View>
       </View>
-      <View>
-        <Text style={tw`text-base text-black/70`}>{title}</Text>
+      <View style={tw`mt-3`}>
+        <Text style={tw`text-sm text-black/70`}>{title}</Text>
+        <Text style={tw`text-sm text-black/70`}>by {artist}</Text>
         <Text style={tw`text-base font-bold text-black/90`}>{showPrice ? utils_formatPrice(price) : "Price on request"}</Text>
         <View style={tw`flex-wrap`}>
           <TouchableOpacity 
-            style={tw`bg-black rounded-full px-5 py-2 w-fit mt-2`}
+            style={tw`bg-black rounded-full px-3 py-1 w-fit mt-2`}
             onPress={() =>
               navigation.push(screenName.purchaseArtwork, {
                 title
@@ -184,7 +186,7 @@ export default function MiniArtworkCard({
             }
             activeOpacity={1}
           >
-            <Text style={tw`text-white text-sm`}>Purchase</Text>
+            <Text style={tw`text-white text-sm font-medium`}>Purchase</Text>
           </TouchableOpacity>
         </View>
       </View>
--- screens/home/components/recentlyViewed/ViewHistoryCard.tsx
+++ screens/home/components/recentlyViewed/ViewHistoryCard.tsx
@@ -1,10 +1,11 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
 import { screenName } from 'constants/screenNames.constants';
 import { getImageFileView } from 'lib/storage/getImageFileView';
 import { colors } from 'config/colors.config';
+import { resizeImageDimensions } from 'utils/utils_resizeImageDimensions.utils';
 
 export default function ViewHistoryCard({
     url,
@@ -21,33 +22,57 @@ export default function ViewHistoryCard({
 
     const image_href = getImageFileView(url, 270);
 
+    const [imageDimensions, setImageDimensions] = useState({
+        width: 200,
+        height: 200,
+      });
+    
+    useEffect(() => {
+        Image.getSize(image_href, (defaultWidth, defaultHeight) => {
+            const { width, height } = resizeImageDimensions(
+            { width: defaultWidth, height: defaultHeight },
+            300
+            );
+            setImageDimensions({ height, width });
+            // setRenderDynamicImage(true);
+        });
+    }, [image_href]);
+
     return (
-        <TouchableOpacity activeOpacity={1} style={styles.container} onPress={() => {
-            navigation.navigate(screenName.artwork, {title: artwork})
-        }}>
-            <View style={styles.imageContainer}>
-                <Image source={{uri: image_href}} style={styles.image} resizeMode="contain" />
-            </View>
-            <View style={styles.mainDetailsContainer}>
-                <Text style={{fontSize: 14, color: colors.primary_black}}>{artwork}</Text>
-                <Text style={{fontSize: 12, color: colors.primary_black, opacity: 0.7, marginTop: 5}}>{artist}</Text>
-            </View>
-        </TouchableOpacity>
+        <View>
+            <View style={{flex: 1}} />
+            <TouchableOpacity 
+                activeOpacity={1} 
+                style={styles.container} 
+                onPress={() => {
+                    navigation.navigate(screenName.artwork, {title: artwork})
+                }}
+            >
+                <Image 
+                    source={{uri: image_href}} 
+                    style={{
+                        width: imageDimensions.width,
+                        height: imageDimensions.height,
+                        borderRadius: 10
+                    }}
+                    resizeMode="cover" 
+                />
+                <View style={styles.mainDetailsContainer}>
+                    <Text style={{fontSize: 14, color: colors.primary_black}}>{artwork}</Text>
+                    <Text style={{fontSize: 12, color: colors.primary_black, opacity: 0.7, marginTop: 5}}>{artist}</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        width: 270,
-        marginLeft: 20
-    },
-    imageContainer: {
-        width: '100%',
-        height: 250
-    },
-    image: {
-        height: '100%',
-        width: '100%',
+        // width: 270,
+        marginLeft: 20,
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: '#f0f0f0'
     },
     mainDetailsContainer: {
         marginTop: 10,
