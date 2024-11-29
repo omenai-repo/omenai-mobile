565ef92e3ae2bd65b4524fc64991c11348758053
ifeanyicodes <ifeanyi@omenai.net>
2024-11-29 13:34:50 +0100

    fixed artwork listing padding issues

--- screens/searchResults/components/resultsListing/ResultsListing.tsx
+++ screens/searchResults/components/resultsListing/ResultsListing.tsx
@@ -3,63 +3,95 @@ import React from "react";
 import { FlatList } from "react-native-gesture-handler";
 import MiniArtworkCard from "components/artwork/MiniArtworkCard";
 import ScrollWrapper from "components/general/ScrollWrapper";
+import { MasonryFlashList } from "@shopify/flash-list";
 
 export default function ResultsListing({ data }: { data: any[] }) {
   return (
-    <ScrollWrapper
-      style={{ marginTop: 20 }}
-      showsVerticalScrollIndicator={false}
-    >
+    // <ScrollWrapper
+    //   style={{ marginTop: 20 }}
+    //   showsVerticalScrollIndicator={false}
+    // >
+    //   <View style={styles.artworksContainer}>
+    //     <FlatList
+    //       data={data[0]}
+    //       renderItem={({ item }: { item: ArtworkFlatlistItem }) => (
+    //         <MiniArtworkCard
+    //           title={item.title}
+    //           url={item.url}
+    //           artist={item.artist}
+    //           showPrice={item.pricing.shouldShowPrice === "Yes"}
+    //           price={item.pricing.usd_price}
+    //           impressions={item.impressions}
+    //           like_IDs={item.like_IDs}
+    //           art_id={item.art_id}
+    //           galleryView
+    //         />
+    //       )}
+    //       keyExtractor={(_, index) => JSON.stringify(index)}
+    //       horizontal={false}
+    //       showsHorizontalScrollIndicator={false}
+    //       // style={{ gap: 200 }}
+    //       contentContainerStyle={{ gap: 30 }}
+    //     />
+    //     <FlatList
+    //       data={data[1]}
+    //       renderItem={({ item }: { item: ArtworkFlatlistItem }) => (
+    //         <MiniArtworkCard
+    //           title={item.title}
+    //           url={item.url}
+    //           artist={item.artist}
+    //           showPrice={item.pricing.shouldShowPrice === "Yes"}
+    //           price={item.pricing.usd_price}
+    //           impressions={item.impressions}
+    //           like_IDs={item.like_IDs}
+    //           art_id={item.art_id}
+    //           galleryView
+    //         />
+    //       )}
+    //       keyExtractor={(_, index) => JSON.stringify(index)}
+    //       horizontal={false}
+    //       showsHorizontalScrollIndicator={false}
+    //       // style={{ gap: 200 }}
+    //       contentContainerStyle={{ gap: 30 }}
+    //     />
+    //   </View>
+    // </ScrollWrapper>
       <View style={styles.artworksContainer}>
-        <FlatList
-          data={data[0]}
+        <MasonryFlashList
+          data={data}
+          estimatedItemSize={400}
+          onEndReachedThreshold={0.1}
           renderItem={({ item }: { item: ArtworkFlatlistItem }) => (
-            <MiniArtworkCard
-              title={item.title}
-              url={item.url}
-              artist={item.artist}
-              showPrice={item.pricing.shouldShowPrice === "Yes"}
-              price={item.pricing.usd_price}
-              impressions={item.impressions}
-              like_IDs={item.like_IDs}
-              art_id={item.art_id}
-            />
+            <View style={{ flex: 1, alignItems: "center", paddingBottom: 20 }}>
+              <MiniArtworkCard
+                title={item.title}
+                url={item.url}
+                artist={item.artist}
+                showPrice={item.pricing.shouldShowPrice === "Yes"}
+                price={item.pricing.usd_price}
+                impressions={item.impressions}
+                like_IDs={item.like_IDs}
+                art_id={item.art_id}
+                galleryView
+              />
+            </View>
           )}
           keyExtractor={(_, index) => JSON.stringify(index)}
-          horizontal={false}
-          showsHorizontalScrollIndicator={false}
-          style={{ gap: 200 }}
-          contentContainerStyle={{ gap: 30 }}
-        />
-        <FlatList
-          data={data[1]}
-          renderItem={({ item }: { item: ArtworkFlatlistItem }) => (
-            <MiniArtworkCard
-              title={item.title}
-              url={item.url}
-              artist={item.artist}
-              showPrice={item.pricing.shouldShowPrice === "Yes"}
-              price={item.pricing.usd_price}
-              impressions={item.impressions}
-              like_IDs={item.like_IDs}
-              art_id={item.art_id}
-            />
-          )}
-          keyExtractor={(_, index) => JSON.stringify(index)}
-          horizontal={false}
-          showsHorizontalScrollIndicator={false}
-          style={{ gap: 200 }}
-          contentContainerStyle={{ gap: 30 }}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          ListFooterComponent={<View style={{paddingTop: 100}} />}
         />
       </View>
-    </ScrollWrapper>
   );
 }
 
 const styles = StyleSheet.create({
   artworksContainer: {
     flexDirection: "row",
-    gap: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 'auto',
+    gap: 15,
     marginTop: 10,
   },
   singleColumn: {
