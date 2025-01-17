c0e686cf6105cc9c9ac6ee17fc10d27b5f642f25
ahmadadejumo <adejumoahmad@gmail.com>
2025-01-17 16:40:04 +0100

    change banner UI in individual screen

--- screens/home/components/banner/BannerCard.tsx
+++ screens/home/components/banner/BannerCard.tsx
@@ -1,11 +1,11 @@
-import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
-import React, { memo } from 'react';
-import { colors } from 'config/colors.config';
-import tw from 'twrnc';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { getPromotionalFileView } from 'lib/storage/getPromotionalsFileView';
-import { fontNames } from 'constants/fontNames.constants';
-import { AntDesign } from '@expo/vector-icons';
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import React, { memo } from "react";
+import { colors } from "config/colors.config";
+import tw from "twrnc";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { getPromotionalFileView } from "lib/storage/getPromotionalsFileView";
+import { fontNames } from "constants/fontNames.constants";
+import { AntDesign } from "@expo/vector-icons";
 
 type BannerItemProps = {
   image?: string;
@@ -15,47 +15,57 @@ type BannerItemProps = {
   handleClick: (url: string) => void;
 };
 
-const { width: windowWidth } = Dimensions.get("window");
+const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
 
-const BannerCard = memo(({
-  image,
-  headline,
-  subheadline,
-  cta,
-  handleClick,
-}: BannerItemProps) => {
-
+const BannerCard = memo(
+  ({ image, headline, subheadline, cta, handleClick }: BannerItemProps) => {
     const image_href = getPromotionalFileView(image, 500);
 
     return (
-        <View style={tw`w-[${windowWidth - 50}px] pl-[15px]`}>
-            <View style={tw`w-full h-full bg-[${colors.primary_black}] rounded-2xl p-[15px] gap-[15px] flex flex-row flex-1`}>
-                <View style={tw`flex-1 h-full space-y-1`}>
-                    <View style={tw`flex-1`}>
-                        <Text style={[tw`font-semibold text-white text-base`, {fontFamily: fontNames.dmSans + 'SemiBold'}]}>{headline}</Text>
-                        <Text style={[tw`mt-2 text-sm text-white/90`, {fontFamily: fontNames.dmSans + 'Regular'}]}>{subheadline}</Text>
-                    </View>
-                    <View style={tw`flex-wrap`}>
-                        <TouchableOpacity
-                            style={tw`bg-white rounded-full h-[40px] w-[40px] mt-3 items-center justify-center `}
-                            onPress={() => handleClick(cta)}
-                            activeOpacity={1}
-                        >
-                            <AntDesign name='arrowright' color={colors.primary_black} size={20} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <View style={tw`flex-1 max-h-[150px] bg-white/20 rounded-md overflow-hidden`}>
-                    <Image
-                        source={{uri: image_href}}
-                        style={tw`w-full h-full`}
-                        resizeMode="cover"
-                    />
-                </View>
+      <View style={tw`w-[${windowWidth - 50}px] pl-[15px]`}>
+        <View
+          style={tw`bg-[#000000] rounded-[10px] p-[25px] flex-row gap-[15px]`}
+        >
+          <View style={tw`flex-1 justify-center`}>
+            <Text
+              style={[
+                tw`font-bold text-[#FFFFFF] text-[15px]`,
+                { fontFamily: fontNames.dmSans + "SemiBold" },
+              ]}
+            >
+              {headline}
+            </Text>
+            <Text
+              style={[
+                tw`mt-2 text-[13px] text-[#FFFFFF]`,
+                { fontFamily: fontNames.dmSans + "Regular" },
+              ]}
+            >
+              {subheadline}
+            </Text>
+            <View
+              style={tw`flex-row items-center mt-[25px] rounded-[26px] bg-[#0000] border-[1px] border-[#fff] px-[5px] py-[10px] justify-center items-center gap-[10px] mr-[35px]`}
+            >
+              <Text style={tw`text-[13px] text-[#fff] font-medium`}>
+                Explore
+              </Text>
+              <AntDesign name="arrowright" color={colors.white} size={15} />
             </View>
-            {/* <View style={tw`flex-1`} /> */}
+          </View>
+          <View style={tw`flex-1 justify-center`}>
+            <Image
+              source={{ uri: image_href }}
+              style={tw.style(`rounded-[4px]`, {
+                width: windowWidth - 280,
+                height: 150,
+              })}
+              resizeMode="cover"
+            />
+          </View>
         </View>
-  );
-})
+      </View>
+    );
+  }
+);
 
-export default BannerCard
+export default BannerCard;
