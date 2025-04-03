fd491691c3a742cd76e49397f5e893d74dad603f
ahmadadejumo <adejumoahmad@gmail.com>
2025-04-03 16:09:07 +0100

    complete dimention details screen

--- components/inputs/NoLabelInput.tsx
+++ components/inputs/NoLabelInput.tsx
@@ -1,13 +1,7 @@
-import {
-  KeyboardTypeOptions,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-} from "react-native";
-import React from "react";
-import { colors } from "config/colors.config";
-import { TextInput } from "react-native-gesture-handler";
+import { KeyboardTypeOptions, StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from 'react';
+import { colors } from 'config/colors.config';
+import { TextInput } from 'react-native-gesture-handler';
 
 type NoLabelInputProps = {
   value: string;
@@ -27,15 +21,13 @@ export default function NoLabelInput({
   handleBlur,
 }: NoLabelInputProps) {
   return (
-    <View style={{ flex: 1, width: "100%" }}>
+    <View style={{ flex: 1, width: '100%' }}>
       <TextInput
         onChangeText={onInputChange}
         placeholder={placeHolder}
         style={[
           styles.container,
-          errorMessage && errorMessage?.length > 0
-            ? { borderColor: "#ff0000" }
-            : null,
+          errorMessage && errorMessage?.length > 0 ? { borderColor: '#ff0000' } : null,
         ]}
         keyboardType={keyboardType}
         autoCapitalize="none"
@@ -49,16 +41,16 @@ export default function NoLabelInput({
 const styles = StyleSheet.create({
   container: {
     height: 60,
-    width: "100%",
+    width: '100%',
     borderWidth: 1,
     borderColor: colors.inputBorder,
-    backgroundColor: "#FAFAFA",
+    backgroundColor: '#FAFAFA',
     paddingHorizontal: 20,
     borderRadius: 5,
     marginTop: 10,
   },
   errorMessage: {
-    color: "#ff0000",
+    color: '#ff0000',
     marginTop: 2,
     width: 300,
   },
--- screens/artist/orders/DimentionsDetails.tsx
+++ screens/artist/orders/DimentionsDetails.tsx
@@ -1,12 +1,122 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+import tw from 'twrnc';
+import BackHeaderTitle from 'components/header/BackHeaderTitle';
+import { uploadArtworkStore } from 'store/gallery/uploadArtworkStore';
+import { validate } from 'lib/validations/upload_artwork_input_validator/validator';
+import Input from 'components/inputs/Input';
+import LongBlackButton from 'components/buttons/LongBlackButton';
+import { SvgXml } from 'react-native-svg';
+import { warningIconSm } from 'utils/SvgImages';
+import { Text } from 'react-native';
 
-const DimentionsDetails = () => {
+type ArtworkDimensionsErrorsType = {
+  height: string;
+  width: string;
+  weight: string;
+};
+
+const DimensionsDetails = () => {
+  const { width } = useWindowDimensions();
+  const { activeIndex, setActiveIndex, artworkUploadData, updateArtworkUploadData } =
+    uploadArtworkStore();
+  const [formErrors, setFormErrors] = useState<ArtworkDimensionsErrorsType>({
+    height: '',
+    width: '',
+    weight: '',
+  });
+
+  const checkIsDisabled = () => {
+    // Check if there are no error messages and all input fields are filled
+    const isFormValid = Object.values(formErrors).every((error) => error === '');
+
+    const areAllFieldsFilled = Object.values({
+      weight: artworkUploadData.weight,
+      height: artworkUploadData.height,
+      width: artworkUploadData.width,
+    }).every((value) => value !== '');
+
+    return !(isFormValid && areAllFieldsFilled);
+  };
+
+  const handleValidationChecks = (label: keyof ArtworkDimensionsErrorsType, value: string) => {
+    if (value.trim() === '') {
+      // If the input is empty, clear the error
+      setFormErrors((prev) => ({ ...prev, [label]: '' }));
+    } else {
+      // Run validation and update error state
+      const { success, errors } = validate(label, value);
+      setFormErrors((prev) => ({ ...prev, [label]: success ? '' : errors[0] }));
+    }
+  };
+
   return (
-    <View>
-      <Text>DimentionsDetails</Text>
+    <View style={tw`flex-1 bg-[#F7F7F7]`}>
+      <BackHeaderTitle title="Dimensions (Including Packaging)" />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={tw`flex-1`}
+      >
+        <ScrollView
+          nestedScrollEnabled={true}
+          style={{ flexGrow: 1 }}
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View style={tw`mt-[30px] mx-[25px] gap-[30px]`}>
+            {(['height', 'width', 'weight'] as Array<keyof ArtworkDimensionsErrorsType>).map(
+              (field) => (
+                <Input
+                  key={field}
+                  label={field.charAt(0).toUpperCase() + field.slice(1)} // Capitalize label
+                  keyboardType="default"
+                  onInputChange={(text) => {
+                    updateArtworkUploadData(field, text);
+                    handleValidationChecks(field as keyof ArtworkDimensionsErrorsType, text);
+                  }}
+                  placeHolder={`Enter ${field}`}
+                  value={artworkUploadData[field]}
+                  errorMessage={formErrors[field]}
+                  containerStyle={{ flex: 0 }}
+                />
+              ),
+            )}
+          </View>
+
+          <View
+            style={tw.style(
+              `border border-[#FFA500] mt-[30px] flex-row items-center gap-[10px] bg-[#FFF3E0] rounded-[8px] p-[15px]`,
+              {
+                marginHorizontal: width / 12,
+              },
+            )}
+          >
+            <SvgXml xml={warningIconSm} />
+            <Text style={tw`text-[14px] text-[#FFA500] font-medium pr-[30px]`}>
+              By accepting this order, you have agreed to have this piece ready for shipping &
+              pickup
+            </Text>
+          </View>
+
+          <View style={tw`mt-[60px] mx-[25px] mb-[150px]`}>
+            <LongBlackButton
+              value="Submit"
+              onClick={() => setActiveIndex(activeIndex + 1)}
+              isLoading={false}
+              isDisabled={checkIsDisabled()}
+            />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 };
 
-export default DimentionsDetails;
+export default DimensionsDetails;
