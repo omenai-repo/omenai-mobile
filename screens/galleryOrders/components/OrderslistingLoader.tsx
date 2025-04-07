45d0a58776f02249cff1bee6a9a19fadbe98586d
ahmadadejumo <adejumoahmad@gmail.com>
2025-04-07 06:58:30 +0100

    add skeleton loader and installed moti

--- screens/galleryOrders/components/OrderslistingLoader.tsx
+++ screens/galleryOrders/components/OrderslistingLoader.tsx
@@ -1,39 +1,36 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import React from 'react';
 import { colors } from 'config/colors.config';
+import { Skeleton } from 'moti/skeleton';
+import tw from 'twrnc';
 import Divider from 'components/general/Divider';
 
 export default function OrderslistingLoader() {
-  const Item = () => {
-    return (
-      <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
-        <View style={{ flex: 1 }}>
-          <View style={{ height: 15, width: 200, backgroundColor: colors.grey50 }} />
-          <View style={{ height: 15, width: 100, backgroundColor: colors.grey50, marginTop: 10 }} />
-        </View>
-        <View style={{ height: 30, width: 100, backgroundColor: colors.grey50 }} />
-      </View>
-    );
-  };
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={[0, 1, 2, 3]}
-        renderItem={() => <Item />}
-        keyExtractor={(_, index) => JSON.stringify(index)}
-        ItemSeparatorComponent={() => (
-          <View style={{ paddingVertical: 30 }}>
-            <Divider />
+  const Item = () => (
+    <View style={tw`p-[20px]`}>
+      <View style={tw`flex-row items-center`}>
+        <View style={tw`flex-row items-center gap-[10px] flex-1`}>
+          {/* Thumbnail */}
+          <Skeleton colorMode="light" height={42} width={42} radius="square" />
+          <View style={tw`gap-[5px]`}>
+            <Skeleton colorMode="light" height={12} width={100} radius="square" />
+            <Skeleton colorMode="light" height={14} width={160} radius="square" />
           </View>
-        )}
-      />
+        </View>
+
+        <Skeleton colorMode="light" height={35} width={35} radius="square" />
+      </View>
     </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 30,
-  },
-});
+  return (
+    <FlatList
+      data={[0, 1, 2, 3, 4, 5, 6]}
+      renderItem={() => <Item />}
+      scrollEnabled={false}
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(_, index) => String(index)}
+      ItemSeparatorComponent={() => <Divider />}
+    />
+  );
+}
