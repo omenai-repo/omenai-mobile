7066fcf26bcebe2830aa7d6600341cb6ae203046
ahmadadejumo <adejumoahmad@gmail.com>
2025-07-02 17:20:02 +0100

    fix UI

--- screens/home/components/featuredArtists/FeaturedArtists.tsx
+++ screens/home/components/featuredArtists/FeaturedArtists.tsx
@@ -41,6 +41,7 @@ export default function FeaturedArtists() {
     image,
     name,
     details,
+    totalLikes,
   }: {
     image: string;
     name: string;
@@ -48,14 +49,30 @@ export default function FeaturedArtists() {
       birthyear: string;
       country: string;
     };
+    totalLikes?: number;
   }) => {
     const image_href = getImageFileView(image, 200);
     return (
       <View style={styles.artistCard}>
         <Image source={{ uri: image_href }} style={styles.artistImage} />
-        <View style={styles.artistInfo}>
-          <Text style={styles.artistName}>{name}</Text>
-          <Text style={styles.artistDetails}>{details.country + ' b.' + details.birthyear}</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <View style={styles.artistInfo}>
+            <Text style={styles.artistName}>{name}</Text>
+            <Text style={styles.artistDetails}>{details.country + ' b.' + details.birthyear}</Text>
+          </View>
+          <View style={{ marginTop: 5 }}>
+            <Text
+              style={{ fontSize: 12, color: '#858585', fontFamily: fontNames.dmSans + 'Regular' }}
+            >
+              {totalLikes || 0} Likes
+            </Text>
+          </View>
         </View>
       </View>
     );
@@ -71,7 +88,9 @@ export default function FeaturedArtists() {
           paddingHorizontal: 20,
         }}
       >
-        <Text style={{ fontSize: 18, fontWeight: '500', flex: 1 }}>Featured Artists</Text>
+        <Text style={{ fontSize: 18, fontWeight: '500', flex: 1 }}>
+          Artists making the rave on Omenai
+        </Text>
       </View>
       <FlatList
         data={artists}
@@ -90,6 +109,7 @@ export default function FeaturedArtists() {
               image={item.mostLikedArtwork.url}
               name={item.artist}
               details={item.mostLikedArtwork}
+              totalLikes={item.totalLikes}
             />
           </TouchableOpacity>
         )}
@@ -104,7 +124,7 @@ export default function FeaturedArtists() {
 
 const styles = StyleSheet.create({
   artistCard: {
-    width: 150,
+    width: 300,
     marginLeft: 20,
   },
   artistImage: {
