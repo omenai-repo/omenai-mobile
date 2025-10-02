d054658bd86ebe7a8d2613dc72e5ca46ae020a9b
ahmadadejumo <adejumoahmad@gmail.com>
2025-10-02 11:08:35 +0100

    update app build number

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
     scheme: 'omenaimobile',
     slug: 'omenai-app',
     owner: 'omenai',
-    version: '1.1.12',
+    version: '1.1.13',
     orientation: 'portrait',
     newArchEnabled: true,
     icon: './assets/icon.png',
@@ -19,7 +19,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.omenai.omenaimobile',
-      buildNumber: '1.1.12',
+      buildNumber: '1.1.13',
       config: {
         googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_IOS,
       },
@@ -33,7 +33,7 @@ export default {
       useNextNotificationsApi: true,
       package: 'com.omenai.omenaimobile',
       permissions: ['android.permission.RECORD_AUDIO'],
-      versionCode: 14,
+      versionCode: 15,
       config: {
         googleMaps: {
           apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_ANDROID,
@@ -49,7 +49,7 @@ export default {
         projectId: 'cfba8b4b-06f7-460a-8fa2-f8671ea18107',
       },
     },
-    runtimeVersion: '1.1.12',
+    runtimeVersion: '1.1.13',
     updates: {
       url: 'https://u.expo.dev/cfba8b4b-06f7-460a-8fa2-f8671ea18107',
     },
