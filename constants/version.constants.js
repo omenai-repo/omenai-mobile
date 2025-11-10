dd1ae66ca431c83328fa75db94da29b2d99651ca
Kelvin Ajayi <239146338+kelvinomenai@users.noreply.github.com>
2025-11-10 18:14:57 +0100

    update: added build extraction by build id cause it was auto pucking production build for submit

--- constants/version.constants.js
+++ constants/version.constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   APP_VERSION: "1.1.23",
   RUNTIME_VERSION: "1.1.23",
-  ANDROID_VERSION_CODE: 34, // EAS will auto-increment from this base value
+  ANDROID_VERSION_CODE: 38, // EAS will auto-increment from this base value
   IOS_BUILD_NUMBER: "1.1.34", // EAS will auto-increment from this base value
 };
 
