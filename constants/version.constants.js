deba53998cd5704b01d1096e0949b5c8f5c34de7
Kelvin Ajayi <239146338+kelvinomenai@users.noreply.github.com>
2025-11-10 15:09:01 +0100

    fix: module not found issue

---
+++ constants/version.constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  APP_VERSION: "1.1.23",
+  RUNTIME_VERSION: "1.1.23",
+  ANDROID_VERSION_CODE: 34, // EAS will auto-increment from this base value
+  IOS_BUILD_NUMBER: "1.1.34", // EAS will auto-increment from this base value
+};
+
--- constants/version.constants.ts
+++
@@ -1,4 +0,0 @@
-export const APP_VERSION = "1.1.23";
-export const RUNTIME_VERSION = "1.1.23";
-export const ANDROID_VERSION_CODE = 34; // EAS will auto-increment from this base value
-export const IOS_BUILD_NUMBER = "1.1.34"; // EAS will auto-increment from this base value
