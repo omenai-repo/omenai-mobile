5513ab37145d1e39718d43b7fd51a58610076ea0
Kelvin Ajayi <239146338+kelvinomenai@users.noreply.github.com>
2025-11-14 15:11:28 +0100

    update: version bump and run build for ios only commented android build

--- constants/version.constants.js
+++ constants/version.constants.js
@@ -1,7 +1,6 @@
 module.exports = {
-  APP_VERSION: "1.1.23",
-  RUNTIME_VERSION: "1.1.23",
+  APP_VERSION: "1.1.24",
+  RUNTIME_VERSION: "1.1.24",
   ANDROID_VERSION_CODE: 38, // EAS will auto-increment from this base value
-  IOS_BUILD_NUMBER: "1.1.34", // EAS will auto-increment from this base value
+  IOS_BUILD_NUMBER: "1.1.35", // EAS will auto-increment from this base value
 };
-
