bf632290b3c85d47275ffacc1c05ab97991257db
Kelvin Ajayi <239146338+kelvinomenai@users.noreply.github.com>
2025-11-17 15:10:16 +0100

    refactor: updated welcome images and added motion logic to all the rows also optimized the tilted grid background component

--- constants/images.constants.ts
+++ constants/images.constants.ts
@@ -6,9 +6,13 @@ import welcomeImage1 from "../assets/images/onboarding/IMG_0101.jpg";
 import welcomeImage2 from "../assets/images/onboarding/IMG_0102.jpg";
 import welcomeImage3 from "../assets/images/onboarding/IMG_0103.jpg";
 import welcomeImage4 from "../assets/images/onboarding/IMG_0104.jpg";
+import welcomeImage5 from "../assets/images/onboarding/IMG_0216.jpg";
+import welcomeImage6 from "../assets/images/onboarding/IMG_0217.jpg";
+import welcomeImage7 from "../assets/images/onboarding/IMG_0218.jpg";
+import welcomeImage8 from "../assets/images/onboarding/IMG_0219.jpg";
 
 export const courselImages = [heroImage, africanArtwork, monalisa, renaissance];
 
 // Images for the tilted grid background
 export const primaryGridImages = [welcomeImage1, welcomeImage2, welcomeImage3, welcomeImage4];
-export const secondaryGridImages = [heroImage, africanArtwork, monalisa, renaissance];
+export const secondaryGridImages = [welcomeImage7, welcomeImage5, welcomeImage6, welcomeImage8];
