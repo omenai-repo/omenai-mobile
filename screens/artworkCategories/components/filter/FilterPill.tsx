b89ef5f8ce37bf268fbaa4ec016f5b77d59c0b36
Kelvin Ajayi <kelvin@omenai.net>
2025-11-29 17:52:58 +0100

    update: converted artwork categories modal to tailwind styles

--- screens/artworkCategories/components/filter/FilterPill.tsx
+++ screens/artworkCategories/components/filter/FilterPill.tsx
@@ -1,52 +1,53 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { Feather } from '@expo/vector-icons'
-import { colors } from 'config/colors.config'
-import { fetchPaginatedArtworks } from 'services/artworks/fetchPaginatedArtworks';
-import { artworkCategoriesStore } from 'store/artworks/ArtworkCategoriesStore';
+import { Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { colors } from "config/colors.config";
+import { fetchPaginatedArtworks } from "services/artworks/fetchPaginatedArtworks";
+import { artworkCategoriesStore } from "store/artworks/ArtworkCategoriesStore";
+import tw from "twrnc";
 
 export default function FilterPill({ filter }: { filter: string }) {
-    const { setArtworks, setIsLoading, setPageCount, pageCount, selectedFilters, removeSingleFilterSelection } = artworkCategoriesStore();
+  const {
+    setArtworks,
+    setIsLoading,
+    setPageCount,
+    pageCount,
+    selectedFilters,
+    removeSingleFilterSelection,
+  } = artworkCategoriesStore();
 
-    const handleRemoveSingleFilter = async () => {
-        setIsLoading(true)
-        if (selectedFilters.length === 1) {
-            removeSingleFilterSelection(filter);
-            const response = await fetchPaginatedArtworks(pageCount, {
-                price: [],
-                year: [],
-                medium: [],
-                rarity: [],
-            });
-            if (response?.isOk) {
-                setArtworks(response.data);
-                setPageCount(response.count);
-            }
-          } else {
-            removeSingleFilterSelection(filter);
-          }
-
-          setIsLoading(false)
+  const handleRemoveSingleFilter = async () => {
+    setIsLoading(true);
+    if (selectedFilters.length === 1) {
+      removeSingleFilterSelection(filter);
+      const response = await fetchPaginatedArtworks(pageCount, {
+        price: [],
+        year: [],
+        medium: [],
+        rarity: [],
+      });
+      if (response?.isOk) {
+        setArtworks(response.data);
+        setPageCount(response.count);
+      }
+    } else {
+      removeSingleFilterSelection(filter);
     }
 
-    return (
-        <TouchableOpacity onPress={handleRemoveSingleFilter}>
-            <View style={styles.container}>
-                <Text style={{fontSize: 14, color: colors.white}}>{filter}</Text>
-                <Feather name='x' size={20} color={colors.white}/>
-            </View>
-        </TouchableOpacity>
-    )
+    setIsLoading(false);
+  };
+
+  return (
+    <TouchableOpacity onPress={handleRemoveSingleFilter}>
+      <View
+        style={[
+          tw`flex-row items-center gap-2.5 py-2.5 px-4 rounded-lg`,
+          { backgroundColor: colors.primary_black },
+        ]}
+      >
+        <Text style={{ fontSize: 14, color: colors.white }}>{filter}</Text>
+        <Feather name="x" size={20} color={colors.white} />
+      </View>
+    </TouchableOpacity>
+  );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: colors.primary_black,
-        borderRadius: 20,
-        gap: 10,
-        paddingHorizontal: 15,
-        paddingVertical: 10
-    }
-})
