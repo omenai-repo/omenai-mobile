5ab1d550e6c5b4ec63549c91e44c9fadaafa1598
Kelvin Ajayi <239146338+kelvinomenai@users.noreply.github.com>
2025-12-08 08:04:02 +0100

    update; fix babel config for importation

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,28 @@ module.exports = function (api) {
       ],
     ],
 
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            components: "./components",
+            config: "./config",
+            constants: "./constants",
+            hooks: "./hooks",
+            navigation: "./navigation",
+            notifications: "./notifications",
+            screens: "./screens",
+            services: "./services",
+            store: "./store",
+            types: "./types",
+            utils: "./utils",
+            assets: "./assets",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
