050ab97f07017c7a5345b81cc337505260f961c3
Kelvin Ajayi <239146338+kelvinomenai@users.noreply.github.com>
2025-12-09 07:20:30 +0100

    fix: use tsconfigPaths for imports, remove module-resolver

--- babel.config.js
+++ babel.config.js
@@ -5,38 +5,10 @@ module.exports = function (api) {
       [
         "babel-preset-expo",
         {
-          unstable_transformImportMeta: true, // ✅ this must go inside the preset array
+          unstable_transformImportMeta: true,
         },
       ],
     ],
-
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          alias: {
-            assets: "./assets",
-            components: "./components",
-            config: "./config",
-            constants: "./constants",
-            data: "./data",
-            hooks: "./hooks",
-            json: "./json",
-            lib: "./lib",
-            navigation: "./navigation",
-            notifications: "./notifications",
-            providers: "./providers",
-            screens: "./screens",
-            scripts: "./scripts",
-            services: "./services",
-            store: "./store",
-            types: "./types",
-            utils: "./utils",
-          },
-        },
-      ],
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
