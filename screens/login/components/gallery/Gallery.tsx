22a1eb5526d6a4857547cf1f8ee895db4cb801f3
Kelvin Ajayi <kelvin@omenai.net>
2025-12-12 16:21:42 +0100

    update: biometric button design

--- screens/login/components/gallery/Gallery.tsx
+++ screens/login/components/gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import LongBlackButton from "../../../../components/buttons/LongBlackButton";
@@ -6,19 +6,38 @@ import Input from "../../../../components/inputs/Input";
 import { useGalleryAuthLoginStore } from "#store/auth/login/GalleryAuthLoginStore";
 import PasswordInput from "#components/inputs/PasswordInput";
 import WithModal from "#components/modal/WithModal";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { screenName } from "#constants/screenNames.constants";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
 import { useLoginHandler } from "#hooks/useLoginHandler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { colors } from "../../../../config/colors.config";
+import { SvgXml } from "react-native-svg";
+import { faceIdIcon } from "#utils/SvgImages";
 
-export default function Gallery() {
-  const { galleryLoginData, setEmail, setPassword, clearInputs, isLoading, setIsLoading } =
-    useGalleryAuthLoginStore();
+type GalleryProps = {
+  biometricProps: {
+    canUseBiometrics: boolean;
+    handleBiometricLogin: () => void;
+    isBiometricLoading: boolean;
+    biometricName: string;
+  };
+};
+
+export default function Gallery({ biometricProps }: GalleryProps) {
+  const {
+    galleryLoginData,
+    setEmail,
+    setPassword,
+    clearInputs,
+    isLoading,
+    setIsLoading,
+  } = useGalleryAuthLoginStore();
   const { handleLogin } = useLoginHandler("gallery");
   const navigation = useNavigation<StackNavigationProp<any>>();
 
-  const handleSubmit = () => handleLogin(galleryLoginData, setIsLoading, clearInputs);
+  const handleSubmit = () =>
+    handleLogin(galleryLoginData, setIsLoading, clearInputs);
 
   return (
     <WithModal>
@@ -39,12 +58,42 @@ export default function Gallery() {
           />
         </View>
         <View style={tw`gap-5`}>
-          <LongBlackButton
-            value={isLoading ? "Loading ..." : "Sign In Gallery"}
-            isDisabled={galleryLoginData.email && galleryLoginData.password ? false : true}
-            isLoading={isLoading}
-            onClick={handleSubmit}
-          />
+          <View style={tw`flex-row gap-3`}>
+            <LongBlackButton
+              value={isLoading ? "Loading ..." : "Sign In Gallery"}
+              isDisabled={
+                galleryLoginData.email && galleryLoginData.password
+                  ? false
+                  : true
+              }
+              isLoading={isLoading}
+              onClick={handleSubmit}
+              style={[
+                biometricProps.canUseBiometrics ? tw`flex-1` : tw`w-full`,
+                { height: 52 },
+              ]}
+            />
+            {biometricProps.canUseBiometrics && (
+              <TouchableOpacity
+                onPress={biometricProps.handleBiometricLogin}
+                disabled={biometricProps.isBiometricLoading}
+                style={[
+                  tw`items-center justify-center rounded-lg h-[52px] w-[52px]`,
+                  { backgroundColor: colors.black },
+                ]}
+              >
+                {biometricProps.biometricName === "Face ID" ? (
+                  <SvgXml xml={faceIdIcon} width={42} height={42} />
+                ) : (
+                  <MaterialCommunityIcons
+                    name="fingerprint"
+                    size={42}
+                    color={colors.white}
+                  />
+                )}
+              </TouchableOpacity>
+            )}
+          </View>
           <TouchableOpacity
             onPress={() =>
               navigation.navigate(screenName.forgotPassword, {
@@ -52,7 +101,9 @@ export default function Gallery() {
               })
             }
           >
-            <Text style={tw`text-sm text-center`}>Forgot password? Click here</Text>
+            <Text style={tw`text-sm text-center`}>
+              Forgot password? Click here
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
