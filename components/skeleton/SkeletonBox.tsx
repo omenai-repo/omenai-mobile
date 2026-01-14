00a22cac736e0465f06587b73cee9df9ab47a20a
Kelvin Ajayi <kelvin@omenai.net>
2026-01-14 21:47:41 +0100

    feat: added skeleton loader

---
+++ components/skeleton/SkeletonBox.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Skeleton } from "moti/skeleton";
+
+type SkeletonBoxProps = {
+  width: number;
+  height: number;
+  radius?: number;
+  colorMode?: "light" | "dark";
+};
+
+/**
+ * Base skeleton component using Moti.
+ * Follows the pattern established in OrderslistingLoader.tsx
+ */
+export default function SkeletonBox({
+  width,
+  height,
+  radius = 4,
+  colorMode = "light",
+}: SkeletonBoxProps) {
+  return (
+    <Skeleton
+      colorMode={colorMode}
+      width={width}
+      height={height}
+      radius={radius}
+    />
+  );
+}
