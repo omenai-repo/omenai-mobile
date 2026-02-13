07f8eb00f008ca51db2f342d4d3469c121626ee3
Kelvin Ajayi <kelvin@omenai.net>
2026-02-13 14:55:40 +0100

    vulnerability fix

--- screens/orders/components/FilterDropdown.tsx
+++ screens/orders/components/FilterDropdown.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { View, Text } from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
 import tw from "twrnc";
 
